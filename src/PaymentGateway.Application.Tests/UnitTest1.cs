84bc6bce6e6c724246481ec2b210b42924caa287
Hoang Anh NGUYEN <hoanganh.nguyen@lseg.com>
2022-06-24 21:20:13 +0200

    Init solution layout

---
+++ src/PaymentGateway.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PaymentGateway.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/PaymentGateway.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PaymentGateway.Domain
+{
+    public class Class1
+    {
+
+    }
+}
