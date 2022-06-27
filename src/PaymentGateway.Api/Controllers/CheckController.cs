c3099af77e7977a4254b38e74fe54c81242b5d97
Hoang Anh NGUYEN <hoanganh.nguyen@lseg.com>
2022-06-27 05:57:53 +0200

    Refactoring

---
+++ src/PaymentGateway.Api/Controllers/CheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaymentGateway.Models;
+
+namespace PaymentGateway.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckController : ControllerBase
+    {
+        [HttpGet()]
+        public string SayHello()
+        {
+            return "I'm Up & Running";
+        }
+    }
+}
